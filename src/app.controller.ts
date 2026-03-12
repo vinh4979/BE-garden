0ccb13f88b5c683cd9af36158e9a55fe8e166548
buiquangvinh <buiquangvinh.0524@gmail.com>
2026-03-13 02:11:14 +0700

    test: auto deploy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
+// import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
